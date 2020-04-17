c4d0b4f246d1a1d2698bd86afe498048867e36fd
Mohammed J Hossain <mjhossainnyc@gmail.com>
2020-04-17 19:07:28 -0400

    3rd App Completed

--- e_card/lib/main.dart
+++ e_card/lib/main.dart
@@ -10,20 +10,90 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.grey[200],
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/myImage.jpg'),
-              ),
-              Text(
-                'Mohammed J Hossain',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 100.0,
                 ),
-              ),
-            ],
+                CircleAvatar(
+                  radius: 80.0,
+                  backgroundImage: AssetImage('images/myImage.jpg'),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Mohammed J Hossain',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'SOFTWARE DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'SourceCodePro',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(20.0),
+                  color: Colors.green,
+                  child: Center(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                        ),
+                        SizedBox(
+                          width: 30.0,
+                        ),
+                        Text(
+                          '+1 (929)990-8890',
+                          style: TextStyle(
+                            fontFamily: 'SourceCodePro',
+                            fontSize: 20.0,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(20.0),
+                  color: Colors.green,
+                  child: Center(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.email,
+                        ),
+                        SizedBox(
+                          width: 30.0,
+                        ),
+                        Text(
+                          'mjhossainnyc@gmail.com',
+                          style: TextStyle(
+                            fontFamily: 'SourceCodePro',
+                            fontSize: 20.0,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
