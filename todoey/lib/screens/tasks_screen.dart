a6e6aa668627d63636a32d39c58bad56ea5af069
Mohammed J Hossain <mjhossainnyc@gmail.com>
2020-05-05 01:22:20 -0400

    App updates

--- todoey/lib/screens/tasks_screen.dart
+++ todoey/lib/screens/tasks_screen.dart
@@ -3,6 +3,73 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.grey[50],
+        child: Icon(
+          Icons.add,
+          color: Colors.green,
+          size: 30.0,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20.0,
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.green,
+                  child: Icon(
+                    Icons.list,
+                    size: 40.0,
+                  ),
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.green),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    '12 Tasks',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
